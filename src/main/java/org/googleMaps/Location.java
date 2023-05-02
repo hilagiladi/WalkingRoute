f6adb02c1ca68903bac7e04ddf1796d166adda2e
Hila Giladi <hilaly55@gmail.com>
2023-05-02 14:39:59 +0300

    adding html and javascript files

--- src/main/java/org/googleMaps/Location.java
+++ src/main/java/org/googleMaps/Location.java
@@ -1,7 +1,5 @@
 package org.googleMaps;
 
-
-
-public class GoogleMap {
+public class Location {
 
 }
--- src/main/java/org/googleMaps/Main.java
+++ src/main/java/org/googleMaps/Main.java
@@ -39,4 +39,6 @@ public class Main {
             e.printStackTrace();
         }
     }
+
+
 }
